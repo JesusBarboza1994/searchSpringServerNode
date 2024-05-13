83dda85b54152f89c5b6d8c6240526a1fc1d7e35
Jesus Barboza <jbarz20@gmail.com>
2024-05-13 11:03:28 -0500

    ADD: cors

--- src/app.js
+++ src/app.js
@@ -3,11 +3,13 @@ import connectDB from "./db/mongooseDB.js";
 import brandRoute from "./routes/brand.route.js"
 import modelCarRoute from "./routes/modelCar.route.js"
 import codeRoute from "./routes/code.route.js"
+import cors from 'cors'
 
 const app = express()
 
 connectDB()
 app.use(express.json()) // Middleware para parsear JSON
+app.use(cors())
 app.use("/brands", brandRoute)
 app.use("/cars",modelCarRoute )
 app.use("/codes",codeRoute )
