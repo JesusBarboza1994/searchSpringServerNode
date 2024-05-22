ab4ec2d4863659a4a1c3db2b66014e2b87ab8f9c
Jesus Barboza <jbarz20@gmail.com>
2024-05-22 16:53:37 -0500

    ADD: customer info in pending order

--- src/services/order/listPendingOrders.service.js
+++ src/services/order/listPendingOrders.service.js
@@ -1,6 +1,24 @@
 import Order from "../../models/order.model.js";
 
 export async function listPendingOrders(){
-  const pendingOrders = await Order.find({status: "PENDIENTE"});
+  // const pendingOrders = await Order.find({status: "PENDIENTE"})
+  const pendingOrders = await Order.aggregate([
+    { $match: { status: "PENDIENTE" } },
+    {
+      $lookup: {
+        from: "customers",
+        localField: "customer_id",
+        foreignField: "_id",
+        as: "customer",
+      },
+    },
+    { $unwind: "$customer" },
+    { $project: { 
+      customer: 1, 
+      cart: 1, 
+      total_amount: 1, 
+      total_items: 1 
+    } },
+  ])
   return pendingOrders
 }
