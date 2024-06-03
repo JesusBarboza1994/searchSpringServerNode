50cd168bedbe78c25b430361d52576b9ae1817f2
titaRuiz1 <asuntita.rl@gmail.com>
2024-06-03 17:04:28 -0500

    chore:message error

--- src/middlewares/appScript.authentication.js
+++ src/middlewares/appScript.authentication.js
@@ -1,10 +1,11 @@
+import config from "../config.js"
 import { CustomError } from "../utils/customError.js"
 
 export default function appScriptAuthentication(req, res, next) {
   try {
     const key =  req.headers['x-api-key']
     console.log("ñaño",req.headers)
-    if(key === process.env.API_KEY) next()
+    if(key === config.appScriptKey) next()
     else throw new CustomError("La llave es incorrecta", 400, 'INVALID_KEY')
   } catch (error) {
   console.log("🚀 ~ appScriptAuthentication ~ error:", error)
