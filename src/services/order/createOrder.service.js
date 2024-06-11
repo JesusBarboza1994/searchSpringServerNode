2ba253c205e57e040dfabb8982d98051b577ddaf
titaRuiz1 <asuntita.rl@gmail.com>
2024-06-11 16:41:48 -0500

    chore:total amount order

--- src/services/order/createOrder.service.js
+++ src/services/order/createOrder.service.js
@@ -3,7 +3,7 @@ import { CustomError } from "../../utils/customError.js";
 
 export async function createOrder({ customer_id, cart }) {
   if(!cart || cart.length == 0 ) throw new CustomError("Cart is empty", 400)
-  const total_amount = cart.reduce((acc, item) => acc + item.price * item.quantity, 0)
+  const total_amount = cart.reduce((acc, item) => acc + item.price * item.quantity/2, 0)
   const total_items = cart.reduce((acc, item) => acc + item.quantity, 0)
   const order = await Order.create({ customer_id, cart, total_amount, total_items })
   return order
