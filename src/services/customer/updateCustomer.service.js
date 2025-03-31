c48c8ad395c3f3138354248287d8f27fcd3c7624
Jesus Barboza <jbarz20@gmail.com>
2025-03-31 16:19:09 -0500

    FIX: update customer service

--- src/services/customer/updateCustomer.service.js
+++ src/services/customer/updateCustomer.service.js
@@ -4,32 +4,31 @@ import Osis from "../../utils/Osis.js";
 export async function updateCustomer({customerId, customer}) {
   const newCustomer = await Customer.findOne({_id: customerId});
   if(customer.document_type == "DNI"){
-    newCustomer.name = customer.name
-    newCustomer.paternal_lastname = customer.paternal_lastname
-    newCustomer.maternal_lastname = customer.maternal_lastname
-    newCustomer.email = customer.email
-    newCustomer.phone = customer.phone
+    if(customer.name) newCustomer.name = customer.name
+    if(customer.paternal_lastname) newCustomer.paternal_lastname = customer.paternal_lastname
+    if(customer.maternal_lastname) newCustomer.maternal_lastname = customer.maternal_lastname
+    if(customer.email) newCustomer.email = customer.email
+    if(customer.phone) newCustomer.phone = customer.phone
     newCustomer.document_type = customer.document_type
-    newCustomer.document_number = customer.document_number
+    if(customer.document_number) newCustomer.document_number = customer.document_number
   }else if(customer.document_type == "RUC"){
-    newCustomer.name = customer.name
-    newCustomer.address = customer.address
-    newCustomer.email = customer.email
+    if(customer.name) newCustomer.name = customer.name
+    if(customer.address) newCustomer.address = customer.address
+    if(customer.email) newCustomer.email = customer.email
+    if(customer.phone) newCustomer.phone = customer.phone
     newCustomer.document_type = customer.document_type
-    newCustomer.phone = customer.phone
-    newCustomer.document_number = customer.document_number
+    if(customer.document_number) newCustomer.document_number = customer.document_number
   }
   await newCustomer.save()
   const existCustomerInOsis = await Osis.getCustomer({document: newCustomer.document_number})
     if(!existCustomerInOsis){
-      await Osis.insertCustomer({
-        documentNumber:newCustomer.document_number,
-        documentType: newCustomer.document_type, 
-        name: newCustomer.name, 
-        maternalLastname: newCustomer.maternal_lastname,
-        paternalLastname:newCustomer.paternal_lastname,
-        address: newCustomer.address, 
-        phone: newCustomer.phone, 
-        email: newCustomer.email})
+      const body = {documentNumber: newCustomer.document_number, documentType: newCustomer.document_type}
+      if(newCustomer.name) body.name = newCustomer.name
+      if(newCustomer.paternal_lastname) body.paternalLastname = newCustomer.paternal_lastname
+      if(newCustomer.maternal_lastname) body.maternalLastname = newCustomer.maternal_lastname
+      if(newCustomer.address) body.address = newCustomer.address
+      if(newCustomer.email) body.email = newCustomer.email
+      if(newCustomer.phone) body.phone = newCustomer.phone
+      await Osis.insertCustomer(body)
     }
 }
