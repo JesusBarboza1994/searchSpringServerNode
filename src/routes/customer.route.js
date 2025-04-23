2eed6cf1abc786059b2819fef36f40ae27c6156f
titaRuiz1 <asuntita.rl@gmail.com>
2025-04-22 21:44:56 -0500

    ADD service get spring

--- src/routes/customer.route.js
+++ src/routes/customer.route.js
@@ -1,8 +1,10 @@
 import express from "express";
 import createOrUpdatePostCustomerController from "../controllers/customer/createOrUpdatePostCustomerController.js";
 import Authentication from "../middlewares/auth.js";
+import getCustomerByIdController from "../controllers/customer/getCustomerById.controller.js";
+
 const router = express.Router();
 
 router.post("/", Authentication, createOrUpdatePostCustomerController);
-
+router.get("/:id",getCustomerByIdController);
 export default router;
